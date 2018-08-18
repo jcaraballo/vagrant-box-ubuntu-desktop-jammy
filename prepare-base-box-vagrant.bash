ea3dbadc1038b15af5afd26be739bd71081a5359
jcaraballo <jcaraballo@users.noreply.github.com>
2018-08-18 19:10:15 +0100

    Append Vangrant insecure key to authorized_keys
    instead of replacing the file. It shouldn't make any difference when
    following these exact rules, but it might be more flexible for people
    which want to keep other public keys in authorized_keys

--- prepare-base-box-vagrant.bash
+++ prepare-base-box-vagrant.bash
@@ -2,6 +2,6 @@
 
 mkdir ~/.ssh
 chmod 700 ~/.ssh
-wget -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
+wget -O - ~/.ssh/authorized_keys >> https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
 chmod 644 ~/.ssh/authorized_keys
 
